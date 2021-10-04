f91e04525318beaec850cf3348429d65a9efdb6c
Victor Hugo Cunha <vhrc@cin.ufpe.br>
2021-10-04 10:43:33 -0300

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
